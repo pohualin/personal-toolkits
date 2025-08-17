9d545c85edd06a0117e203c8e63a4fa4caded298
Paul Lin <po_hua_lin@bcbsil.com>
2025-08-17 09:30:00 -0500

    update test cron to send gmail

--- scripts/test_cron.py
+++ scripts/test_cron.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
 import datetime
+from util.gmail_util import send_gmail
 
-print(f"Hello World! - {datetime.datetime.now()}")
+subject = "Test cron job"
+body = f"Hello World! - {datetime.datetime.now()}"
+to = "pohualin@gmail.com"
+
+send_gmail(subject=subject, body=body, to=to)
