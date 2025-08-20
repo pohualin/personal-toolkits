e6da4ed2e792aa6fba51a16533afa8d12a33ce1f
Paul Lin <po_hua_lin@bcbsil.com>
2025-08-19 22:05:33 -0500

    update deploy_to_jmusk

--- deploy_to_jmusk.sh
+++ deploy_to_jmusk.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Copy secrets to remote host
-scp .env token.json jmusk@192.168.2.102:/home/jmusk/Workspace/python/personal-toolkits/
+scp .env token.json sheet-token.json jmusk@192.168.2.102:/home/jmusk/Workspace/python/personal-toolkits/
 
 # Build and run Docker Compose on remote host
 ssh jmusk@192.168.2.102 "cd /home/jmusk/Workspace/python/personal-toolkits && docker compose build && docker compose up -d"
