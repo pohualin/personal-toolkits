23ebd8e82c66bb670422c0851f4775ce4ad16f4b
Paul Lin <po_hua_lin@bcbsil.com>
2025-08-23 09:37:05 -0500

    clean up docker sh scripts

--- docker-run.sh
+++ docker-run.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 set -e
-
 # Build the Docker image
 docker compose build
-
 # Run the container
 docker compose up -d
-
-# Show running containers
-# docker compose ps
-
-# Show logs
-# docker compose logs -f
